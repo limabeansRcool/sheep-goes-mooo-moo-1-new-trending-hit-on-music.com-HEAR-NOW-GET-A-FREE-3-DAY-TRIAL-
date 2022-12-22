01fa2ec067ba63307fba519de7e045b7877b4543
limabeansRcool <102268583+limabeansRcool@users.noreply.github.com>
2022-12-21 20:55:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function identifynoise() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    HI=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/3m8LF01s1/model.json",modelrrrrr);
+
+}
+function modelrrrrr() {
+    HI.classify(gotresult);
+
+}
+
+function gotresult(error, results) {
+//i dont know what to put here, oh ee ere, yaya ya yay 
+//ba ba ba sheep goes noise sound, fallen down down don't know why im rhyming
+//lieing
+//because sheep goes MOO MOOO MOO MOO
+
+
+}
